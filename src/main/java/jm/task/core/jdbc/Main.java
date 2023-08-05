747f61a886bca2c31bc11c9767a82cb96b2d9036
Igor <igorancemzik@gmail.com>
2023-08-05 11:53:49 +0500

    fix try-finally with shutdown

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -6,14 +6,17 @@ import jm.task.core.jdbc.util.Util;
 public class Main {
     public static void main(String[] args) {
         UserServiceImpl userService = new UserServiceImpl();
-        userService.createUsersTable();
-        userService.saveUser("Igor", "Astashkin", (byte) 32);
-        userService.saveUser("Sergey", "Astashkin", (byte) 54);
-        userService.saveUser("Semen", "Astashkin", (byte) 24);
-        userService.saveUser("Denis", "Astashkin", (byte) 40);
-        userService.getAllUsers().forEach(System.out::println);
-        userService.cleanUsersTable();
-        userService.dropUsersTable();
-        Util.shutdown();
+        try {
+            userService.createUsersTable();
+            userService.saveUser("Igor", "Astashkin", (byte) 32);
+            userService.saveUser("Sergey", "Astashkin", (byte) 54);
+            userService.saveUser("Semen", "Astashkin", (byte) 24);
+            userService.saveUser("Denis", "Astashkin", (byte) 40);
+            userService.getAllUsers().forEach(System.out::println);
+            userService.cleanUsersTable();
+            userService.dropUsersTable();
+        } finally {
+            Util.shutdown();
+        }
     }
 }
